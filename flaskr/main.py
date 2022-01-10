2cb443c169785827030f4c42c4161a9d1d83710e
Sho Emoto <dd2d00seg1@gmail.com>
2022-01-10 17:34:26 +0900

    pythonファイルからデータを渡して表示する(jinja2のテンプレートエンジン)

--- flaskr/main.py
+++ flaskr/main.py
@@ -3,4 +3,9 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    book = {
+        'title': 'はらぺこあおむし',
+        'price': 1200,
+        'arrival_day': '2020年7月12日'
+    }
+    return render_template('index.html', book=book)

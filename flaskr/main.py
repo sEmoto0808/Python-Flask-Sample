aafe447f70947e9fa7de82b4d1238e4d63c4d642
Sho Emoto <dd2d00seg1@gmail.com>
2022-01-10 18:11:16 +0900

    dbからデータを抽出

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,18 +1,23 @@
 from flaskr import app
 from flask import render_template
+import sqlite3
+
+DATABASE = 'database.db'
 
 
 @app.route('/')
 def index():
-    books = [{
-        'title': 'はらぺこあおむし',
-        'price': 1200,
-        'arrival_day': '2020年7月12日'
-    }, {
-        'title': 'ぐりとぐら',
-        'price': 990,
-        'arrival_day': '2020年7月13日'
-    }]
+    connect = sqlite3.connect(DATABASE)
+    db_books = connect.execute('SELECT * FROM books').fetchall()
+    connect.close()
+
+    books = []
+    for row in db_books:
+        books.append({
+            'title': row[0],
+            'price': row[1],
+            'arrival_day': row[2]
+        })
     return render_template(
         'index.html',
         books=books
